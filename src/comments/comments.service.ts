b7f61fe7fb99ea0c9abf7e67b31493cb04850ab0
AlexGr <753464@gmail.com>
2022-12-17 01:31:22 +0300

    1.0.5

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -12,7 +12,7 @@ export class CommentsService {
     ) { }
     async findOne(userId) {
 
-        CommentSchema.virtual('myStatus', { ref: 'Likes', localField: '_id', foreignField: 'commentId', options: { match: { userId: userId } } })//TODO всунуть в метод .Метод в модель и вызывать создание виртуальных полей. 
-        return await this.CommentModel.findOne().populate('myStatus', 'myStatus')
+        CommentSchema.virtual('likesInfo.myStatus', { ref: 'Likes', localField: '_id', foreignField: 'commentId', options: { match: { userId: userId } } })//TODO всунуть в метод .Метод в модель и вызывать создание виртуальных полей. 
+        return await this.CommentModel.findOne().populate('likesInfo.myStatus', 'myStatus')
     }
 }
